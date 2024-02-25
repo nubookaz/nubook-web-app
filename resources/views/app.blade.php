2865a0329b434fdcac8b883dcbeffc1990ec33a8
Levi Elizaga <42102283+nuvinevisuals@users.noreply.github.com>
2024-02-25 14:35:44 -0700

    updated welcome page
    updated css and retrieved lost header nav

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="antialiased bg-black overflow-hidden">
+    <body class="antialiased bg-black overflow-scroll">
         @inertia
     </body>
 </html>
